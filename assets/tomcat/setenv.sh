5bf0aeafcd4ce357ad534ebf01f050a89100b4dc
nickmcmahonHOD <nick.mcmahon@digital.homeoffice.gov.uk>
2017-12-20 21:19:15 +0000

    env change to medium

--- assets/tomcat/setenv.sh
+++ assets/tomcat/setenv.sh
@@ -5,7 +5,7 @@ JAVA_OPTS="$JAVA_OPTS -Dalfresco.home=/usr/local/alfresco"
 JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF-8"
 JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote"
 JAVA_OPTS="$JAVA_OPTS -XX:ReservedCodeCacheSize=128m"
-JAVA_OPTS="$JAVA_OPTS -Xms150m -Xmx2304m -Djavax.net.ssl.trustStore=/data/truststore.jks" # java-memory-settings
+JAVA_OPTS="$JAVA_OPTS -Xms1536m -Xmx4096m -Djavax.net.ssl.trustStore=/data/truststore.jks" # java-memory-settings
 
 # service:jmx:rmi:///jndi/rmi://192.168.1.33:9999/jmxrmi
 #JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.rmi.port=9999"
