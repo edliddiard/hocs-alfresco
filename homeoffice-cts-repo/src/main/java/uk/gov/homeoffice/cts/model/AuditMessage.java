67e2a1c5ace381b797b4d0f702d19645b57bfc67
nickmcmahonHOD <nick.mcmahon@digital.homeoffice.gov.uk>
2018-02-22 21:23:31 +0000

    Dial back Alfresco DB pool + force data to UTF-8.

--- homeoffice-cts-repo/src/main/java/uk/gov/homeoffice/cts/model/AuditMessage.java
+++ homeoffice-cts-repo/src/main/java/uk/gov/homeoffice/cts/model/AuditMessage.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.alfresco.service.namespace.QName;
 
 import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +26,7 @@ public class AuditMessage {
     @JsonProperty(value="data")
     private final Map<String, String> afterMap;
 
-    public AuditMessage(Map<QName, Serializable> after) {
+    public AuditMessage(Map<QName, Serializable> after) throws UnsupportedEncodingException {
         uuid = UUID.randomUUID();
         timestamp = new Date();
         afterMap = transformMap(after);
@@ -72,12 +73,18 @@ public class AuditMessage {
         return sb.toString();
     }
 
-    private static Map<String, String> transformMap(Map<QName, Serializable> map){
+    private static Map<String, String> transformMap(Map<QName, Serializable> map) throws UnsupportedEncodingException {
         Map<String, String> retMap = new HashMap<>();
 
         for(Map.Entry<QName, Serializable> entry : map.entrySet()) {
             System.out.print(entry);
-            retMap.put(entry.getKey().getLocalName(), entry.getValue() == null ? "null" : entry.getValue().toString());
+            String value = "null";
+            if(entry.getValue() != null) {
+                byte[] utf8data = entry.getValue().toString().getBytes("UTF-8");
+                value = new String(utf8data, "UTF-8");
+            }
+
+            retMap.put(entry.getKey().getLocalName(), value );
         }
 
         return retMap;
