af5ffc68a4ab94da48119873ca9120a87ca814b8
nickmcmahonHOD <nick.mcmahon@digital.homeoffice.gov.uk>
2018-03-06 15:10:17 +0000

    Testing with libreoffice

--- homeoffice-cts-repo/src/main/amp/config/alfresco/extension/templates/webscripts/homeoffice/cts/util/importUsersandGroups.post.js
+++ homeoffice-cts-repo/src/main/amp/config/alfresco/extension/templates/webscripts/homeoffice/cts/util/importUsersandGroups.post.js
@@ -184,7 +184,7 @@ function createPerson(username, firstName, secondname, email, password, groupNam
     for (var g = 0; g < groupNameArray.length; g++) {
         //var groupFullName = 'GROUP_' + groupNameArray[g];
         var groupFullName = groupNameArray[g];
-        if (people.getGroup(groupFullName) != null && !isInGroup(groupFullName, username) {
+        if (people.getGroup(groupFullName) != null && !isInGroup(groupFullName, username)) {
             people.addAuthority(people.getGroup(groupFullName), person);
             person.properties["{http://cts-beta.homeoffice.gov.uk/model/user/1.0}passwordExpiryDate"] = expireDate;
             person.save();
